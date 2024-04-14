b4cab44eb10024230d0a5fb314a147e87d195876
Himanshu <himanshutiwari199@gmail.com>
2024-04-14 13:01:50 +0530

    setup code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
+import Loginpage from './components/Loginpage';
+import Signup from './components/Signup';
+import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+
+
+
 
 function App() {
+  const [showLoginPage, setShowLoginPage] = useState(false);
+
+  const toggleLoginPage = () => {
+    setShowLoginPage(!showLoginPage);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+          <div className="login-page-overlay">
+            <Routes>
+              <Route path="login" element={<Loginpage />} />
+              <Route path="signup" element={<Signup />} />
+              <Route path="/" element={<Home/>}/>
+            </Routes>
+          </div>
+        
+      </div>
+      
+    </>
   );
 }
 
