ee85b0f0b100e36f2530f2367722baa86b8496cb
him-20-05 <123268088+him-20-05@users.noreply.github.com>
2024-04-16 16:42:57 +0530

    Update Movies.js

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -35,7 +35,6 @@ export default function Movies() {
       <div className="row row-cols-1 row-cols-md-2 g-4">
         {movie.map((currMovie) => {
           const { imdbID, Title, Poster, Year } = currMovie;
-          //const movieName = Title.substring(0,15)
           return (
             <div className="col" key={imdbID}>
               <div className="card h-100">
