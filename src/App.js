0f53e08ff2b0a01635b4522e9c69a9732ebae4df
him-20-05 <123268088+him-20-05@users.noreply.github.com>
2024-04-17 03:07:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,32 +1,30 @@
 import React from "react";
-import Loginpage from './pages/Loginpage';
-import Signup from './pages/Signup';
+import Loginpage from "./pages/Loginpage";
+import Signup from "./pages/Signup";
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Watchlist from "./components/Watchlist";
-
-
-
+import { ToastContainer } from "react-toastify";
 
 function App() {
-  
-
   return (
     <>
+      <ToastContainer
+        theme="colored"
+        className="d-flex justify-content-center align-items-center"
+      />
+
       <div>
-          
-            <Routes>
-              <Route path="/login" element={<Loginpage />} />
-              <Route path="/login/signup" element={<Signup />} />
-              <Route path="/" element={<Home/>}/>
-              <Route path="/watchlist" element={<Watchlist/>} />
-            </Routes>
-          </div>
-        
-      
-      
+        <Routes>
+          <Route path="/login" element={<Loginpage />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/watchlist" element={<Watchlist />} />
+        </Routes>
+      </div>
     </>
   );
 }
 
 export default App;
+
